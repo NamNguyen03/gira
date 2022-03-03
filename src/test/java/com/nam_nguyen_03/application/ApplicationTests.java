d28a1608644cd6bce8cfaddc3016eee380868f0e
Nam Nguyen <nam03031999@gmail.com>
2022-03-03 10:07:53 -0400

    init app

---
+++ src/test/java/com/nam_nguyen_03/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nam_nguyen_03.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
