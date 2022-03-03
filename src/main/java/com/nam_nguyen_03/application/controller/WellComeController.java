0def75c677031d650fedc9905234fec71673c4b5
Nam Nguyen <nam03031999@gmail.com>
2022-03-03 10:11:48 -0400

    add version

---
+++ src/main/java/com/nam_nguyen_03/application/controller/WellComeController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.nam_nguyen_03.application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author nam
+ *
+ */
+@RestController
+public class WellComeController {
+	
+	@GetMapping()
+	public String wellcome() {
+		return "Hello";
+	}
+}
